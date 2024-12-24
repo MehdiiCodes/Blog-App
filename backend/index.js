d2f2e88b0091e98e80fa233046549546986bd76f
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-12-25 01:18:23 +0530

    blogApp

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+// Import required modules
+const express = require('express');
+const cors = require('cors');
+const UserRouter = require('./routers/UserRouter');
+
+// Initialize the app
+const app = express();
+
+// Middleware
+app.use(express.json()); // For parsing JSON data
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+
+
+// Define routes
+app.use('/user', UserRouter); // Routers for user management
+app.use('/post', PostRouter); // Routers for posts (like creating, deleting posts)
+app.use('/community', CommunityRouter); // Routers for community (like creating, deleting communites)
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Social Media API!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
